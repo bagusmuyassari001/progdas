772c2596f6b4da987b36d7f144b9453181b8063b
bagusmuyassari001 <blsmk.250127018@budiluhur.sch.id>
2025-11-06 14:31:09 +0700

    progdas html pake js

---
+++ script.js
@@ -0,0 +1,22 @@
+//biar tombol landing page smooth
+const learnBtn = document.querySelector('.btn[href="#testimoni"]');
+if (learnBtn) {
+    learnBtn.addEventListener('click', e =>   {
+        e.preventDefault();
+        const target = document.querySelector("#testimoni");
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth'});
+        }
+    })
+}
+
+
+//notifkasi setalah submit form kontak
+const form = document.querySelector(".contact-form");
+if (form) {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        alert("Pesan berhasil terkirim!");
+        form.reset();
+    } )
+}
